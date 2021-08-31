3e49bb7c5ee0ea318ec6409672a44abbac4722e9
Lucas Maidana <lucas.maidana@globant.com>
2021-08-31 11:24:21 -0300

    Add Router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from "vue";
+import App from "./App.vue";
+import About from "./About.vue";
 
-createApp(App).mount('#app')
+const routes = {
+  "/": App,
+  "/about": About,
+};
+
+const SimpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname,
+  }),
+
+  computed: {
+    CurrentComponent() {
+      return routes[this.currentRoute] || NotFoundComponent;
+    },
+  },
+
+  render() {
+    return h(this.CurrentComponent);
+  },
+};
+
+createApp(SimpleRouter).mount("#app");
